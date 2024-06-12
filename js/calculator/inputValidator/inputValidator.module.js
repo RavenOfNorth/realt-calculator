881c3d4726eba8cc751b7011fcb6bbd1cc10a0f6
RavenOfNorth <dhaurylenka.job@yandex.by>
2024-06-13 01:06:43 +0300

    v.0.0.2c
    The validator bugs have been fixed

--- js/calculator/inputValidator/inputValidator.module.js
+++ js/calculator/inputValidator/inputValidator.module.js
@@ -1,11 +1,11 @@
 export function inputValidator() {
-    const flatSquare = Number(document.getElementById("square").value);
-    const flatCost = Number(document.getElementById("cost-dollar").value);
+    const flatSquare = document.getElementById("square").value;
+    const flatCost = document.getElementById("cost-dollar").value;
 
-   if (flatSquare <=0) {
+   if (Number(flatSquare) <=0 || flatSquare === "") {
        alert("Введите коректную площадь квартиры");
        return false;
-   } else if (flatCost.value <= 0) {
+   } else if (Number(flatCost) <= 0 || flatCost === "") {
        alert("Пожалуйста, введите корректное значение для стоимости квартиры.")
        return false;
    } else {
