9b342a4d0fc477bac811b1f6109fc46d8d869b35
RavenOfNorth <dhaurylenka.job@yandex.by>
2025-01-14 21:18:21 +0300

    update

--- js/calculator/inputValidator/inputValidator.module.js
+++ js/calculator/inputValidator/inputValidator.module.js
@@ -1,11 +1,7 @@
 export function inputValidator() {
-    const flatSquare = document.getElementById("square").value;
     const flatCost = document.getElementById("cost-dollar").value;
 
-   if (Number(flatSquare) <=0 || flatSquare === "") {
-       alert("Введите коректную площадь квартиры");
-       return false;
-   } else if (Number(flatCost) <= 0 || flatCost === "") {
+   if  (Number(flatCost) <= 0 || flatCost === "") {
        alert("Пожалуйста, введите корректное значение для стоимости квартиры.")
        return false;
    } else {
